766afb2b211b288f19df2438bf37e4c8848f0fad
ZLoganZ <ln26805@gmail.com>
2023-11-02 23:25:18 +0700

    pp

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -14,8 +14,8 @@ const HEADER = {
 const avt_default =
   'https://res.cloudinary.com/dp58kf8pw/image/upload/c_pad,b_auto:predominant,fl_preserve_transparency/v1694576962/default_avatar_ixpwcf.jpg';
 
-const pp_UserDefault = '_id name email user_image';
-const se_UserDefault = ['_id', 'name', 'email', 'user_image'];
+const pp_UserDefault = '_id name email user_image last_online';
+const se_UserDefault = ['_id', 'name', 'email', 'user_image', 'last_online'];
 
 const se_UserDefaultForPost = [
   '_id',
