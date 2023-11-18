22f5404848c9aaa572f59dcdaf541cea6b7a4dfe
ZLoganZ <ln26805@gmail.com>
2023-11-18 10:56:05 +0700

    Update message model enum options

--- src/models/message.model.js
+++ src/models/message.model.js
@@ -11,7 +11,7 @@ const MessageSchema = new Schema(
     conversation_id: { type: ObjectId, ref: 'Conversation', required: true },
     type: {
       type: String,
-      enum: ['text', 'notification', 'audio', 'file', 'voice', 'video'],
+      enum: ['text', 'image', 'notification', 'audio', 'file', 'voice', 'video'],
       default: 'text'
     },
     images: { type: [String], default: null },
